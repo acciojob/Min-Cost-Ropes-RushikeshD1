5a377cc4f1109efffefa7bacbfa39502eb65f419
RushikeshD1 <141423104+RushikeshD1@users.noreply.github.com>
2026-02-27 16:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	if (arr.length < 2){
+		return arr[0];
+	}
+	
+	arr.sort((a,b)=> a-b);
+	// 2 3 4 6
+	// 0 1 2 3
+	let sum = arr[0] + arr[1] // 5 + 9 + 15
+
+	for(let i=2; i<arr.length; i++){// 4
+		let ans = 0 // 9
+		ans = sum + arr[i];
+		sum += ans
+	}
+
+	return sum;
   
 }
 
